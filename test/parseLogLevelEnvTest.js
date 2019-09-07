0b41ecca85b27f8f73ebdf92e0bb6602436cd5a4
Michael Scherer <michael.scherer@sealsystems.de>
2019-09-07 08:57:32 +0200

    Use new interface of nodeenv

--- test/parseLogLevelEnvTest.js
+++ test/parseLogLevelEnvTest.js
@@ -1,7 +1,7 @@
 'use strict';
 
 const assert = require('assertthat');
-const nodeenv = require('nodeenv');
+const nodeenv = require('nodeenv').default;
 
 const parse = require('../lib/parseLogLevelEnv');
 
