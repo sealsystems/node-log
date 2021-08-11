50814fd08a67bcf1f018c4bca5f7b2d7e52f4488
Thomas Lagies <thomas.lagies@sealsystems.de>
2021-08-11 16:23:10 +0200

    added tests for metadata transformation

--- test/loggerTest.js
+++ test/loggerTest.js
@@ -32,11 +32,35 @@ suite('logger', () => {
   });
 
   test('log message', async () => {
-    logger.getLogger().info('test');
+    logger.getLogger().info('test', 'noch ein test string');
     assert.that(console.log.calledOnce).is.true();
     const logMessage = console.log.getCall(0).args[0];
     const logEntry = JSON.parse(logMessage);
     assert.that(logEntry.isoTimestamp).is.ofType('string');
     assert.that(logEntry.isoTimestamp).is.equalTo(new Date(logEntry.timestamp).toISOString());
+  });
+
+  test('log message, transform string to object', async () => {
+    logger.getLogger().info('test', 'noch ein test string');
+    assert.that(console.log.calledOnce).is.true();
+    const logMessage = console.log.getCall(0).args[0];
+    const logEntry = JSON.parse(logMessage);
+    assert.that(logEntry.metadata).is.ofType('object');
+  });
+
+  test('log message, transform array to object', async () => {
+    logger.getLogger().info('test', ['element1', 'element2', 'element3']);
+    assert.that(console.log.calledOnce).is.true();
+    const logMessage = console.log.getCall(0).args[0];
+    const logEntry = JSON.parse(logMessage);
+    assert.that(logEntry.metadata).is.ofType('object');
+  });
+
+  test('log message, log object as is', async () => {
+    logger.getLogger().info('test', { statusCode: 500 });
+    assert.that(console.log.calledOnce).is.true();
+    const logMessage = console.log.getCall(0).args[0];
+    const logEntry = JSON.parse(logMessage);
+    assert.that(logEntry.metadata).is.ofType('object');
   });
 });
